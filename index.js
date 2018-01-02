c63a6bc602a8d034efcbdcaa4f080fed41f08abb
root <root@ip-132-148-67-154.ip.secureserver.net>
2018-01-02 09:47:11 -0700

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require( 'fs' );
+var app = require('express')();
+var https        = require('https');
+
+var server = https.createServer({ 
+                  key: fs.readFileSync('/etc/letsencrypt/live/experto-e.com-0001/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/experto-e.com-0001/cert.pem')
+             },app);
+server.listen(3000);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+  socket.on('chat-message', function(msg,nick,user,canal){
+    io.emit('chat-message-'+canal, msg,nick,user,canal);
+  });
+
+ socket.on('cierra', function(mensage,canal){
+    io.emit('cierra-'+canal, mensage);
+  });
+});
