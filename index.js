ebd688bee00f91f085c0a767a4058bc8fdf5fcb0
houltman <houltman_gonzalez@hotmail.com>
2024-05-22 12:25:37 -0400

    fix: test demo socket.io

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ var fs = require("fs");
 var app = require("express")();
 var https = require("https");
 
+// Deploy en server agegar las keys de letsencrypt
+/*
 var server = https.createServer(
   {
     key: fs.readFileSync(
-     // "/etc/letsencrypt/live/experto-e.com-0001/privkey.pem"
+      "/etc/letsencrypt/live/experto-e.com-0001/privkey.pem"
     ),
     cert: fs.readFileSync(
-     // "/etc/letsencrypt/live/experto-e.com-0001/cert.pem"
+      "/etc/letsencrypt/live/experto-e.com-0001/cert.pem"
     ),
   },
   app
 );
+server.listen(3000);
+*/
+
+var http = require('http');
+var server = http.createServer(app);
 server.listen(3000);
 
 var io = require("socket.io").listen(server);
